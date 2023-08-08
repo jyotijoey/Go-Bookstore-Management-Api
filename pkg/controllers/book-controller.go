39353e3269ba69c00eb9ee5d4f96eba89e415981
Jyoti Burnwal <jburnwal5@gmail.com>
2023-08-08 23:49:46 +0530

    restructured code

--- pkg/controllers/book-controller.go
+++ pkg/controllers/book-controller.go
@@ -8,24 +8,24 @@ import (
 
 var NewBooks models.Book
 
-// var (
-	func GetBook(w http.ResponseWriter, r *http.Request)  {
+var (
+	GetBook = func(w http.ResponseWriter, r *http.Request)  {
 		newBooks := models.GetAllBooks()
 		res, _ := json.Marshal(newBooks)
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
 		w.Write(res)
 	}
-	func CreateBook(w http.ResponseWriter, r *http.Request)  {
+	CreateBook = func(w http.ResponseWriter, r *http.Request)  {
 
 	}
-	func GetBookById(w http.ResponseWriter, r *http.Request)  {
+	GetBookById = func(w http.ResponseWriter, r *http.Request)  {
 
 	}
-	func UpdateBookById(w http.ResponseWriter, r *http.Request)  {
+	UpdateBookById = func(w http.ResponseWriter, r *http.Request)  {
 
 	}
-	func DeleteBook(w http.ResponseWriter, r *http.Request)  {
+	DeleteBook = func(w http.ResponseWriter, r *http.Request)  {
 
 	}
-// )
+)
